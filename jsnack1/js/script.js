ce2c1aa53c1f818ed85b1d6aef15114bdb336520
federico_viganò <fede_viga@hotmail.it>
2024-02-12 12:14:33 +0100

    creata funzione che inverte la parola

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,2 +1,26 @@
 // Snack 1:
 // Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+
+// inizializzo elementi HTML e variabili
+const inputElement = document.querySelector("#input");
+const submitButtonElement = document.querySelector("#submit");
+const resultElement = document.querySelector("#result");
+let userWord = "ciao";
+
+
+
+let result = mirror(userWord);
+console.log(result);
+
+
+// funzione che specchia la parola
+function mirror (word) {
+    
+    let mirroredWord = "";
+    // inverto word con un ciclo for
+    for (let i = 0; i < word.length; i++) {
+        mirroredWord += word[word.length - 1 - i];
+    }
+    return mirroredWord;
+}
