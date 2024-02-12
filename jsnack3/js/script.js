3776fb3b8463cfd49ff425aca830c8022b6430f8
federico_viganò <fede_viga@hotmail.it>
2024-02-12 13:33:37 +0100

    completato snack 3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -8,9 +8,31 @@
 // Nel nostro caso, se la chiamassimo così tagliaArray(arrayNumeri, 2, 4) ci restituirebbe un array così composto:
 // [2, 3, 4]
 
+// inizializzo elementi HTML  e variabili
+const arrayLenghtElement = document.querySelector("#array-lenght");
+const arrayMinElement = document.querySelector("#array-min");
+const arrayMaxElement = document.querySelector("#array-max");
+const buttonElement = document.querySelector("#button");
+const resultElement = document.querySelector("#result");
 
-const arrayNumeri = [0, 1, 2, 3, 4, 5, 6];
+// al click del bottone genero array della lunghezza desiderata dall'utente e lo taglio in base al min e max inseriti
+buttonElement.addEventListener('click', function(){
+    const arraylenght = arrayLenghtElement.value;
+    const arrayMin = arrayMinElement.value;
+    const arrayMax = arrayMaxElement.value;
+    const userArray = [];
+    let resultArray = [];
 
+    // ciclo for per generare array della lunghezza desiderata
+    for (let i = 0; i < arraylenght; i++) {
+        userArray.push(i)
+    }
+
+    // invoco funzione per tagliare array e stampo risultato in pagina
+    resultArray = cutArray(userArray, arrayMin, arrayMax);
+    resultElement.innerHTML = "Array generato: " + userArray + "<br> Array tagliato dalla posizione " + arrayMin + " alla posizione " + arrayMax + " : " + resultArray;
+
+})
 
 // funzione che taglia l'array in base al min e max scelti
 function cutArray(array, min, max) {
@@ -21,5 +43,3 @@ function cutArray(array, min, max) {
     }
     return resultArray;
 }
-
-console.log(cutArray(arrayNumeri, 2, 4));
