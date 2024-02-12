ea893fe623ca87bdc77003286396729dcf142fd6
federico_viganò <fede_viga@hotmail.it>
2024-02-12 12:38:35 +0100

    creato ciclo for per unire e mixare gli array

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,3 +1,17 @@
 // Snack 2:
 // Scrivi una funzione che fonda due array presi come parametri (date per scontato che abbiano lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+const array1 = ["a", "b", "c"];
+const array2 = [1, 2, 3];
+let result = [];
+
+
+// ciclo for che unisce i due array
+for (i = 0; i < array1.length; i++) {
+    result.push(array1[i]);
+    result.push(array2[i]);
+}
+
+console.log(result);
