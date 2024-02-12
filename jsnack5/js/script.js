66af7e75228a18769064b446c232778e3bcea101
federico_viganò <fede_viga@hotmail.it>
2024-02-12 16:52:02 +0100

    creata funzione per rendere maiuscola la prima lettera

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,3 +1,20 @@
 // Snack 5 (bonus):
 // Scrivi una funzione che accetti una stringa come parametro e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
 // Es: funzioneEsempio("nel mezzo del cammin di nostra vita")  :freccia_destra:  "Nel Mezzo Del Cammin Di Nostra Vita"
+
+
+const userString = "nel mezzo del cammin di nostra vita";
+
+console.log(upperCaseWords(userString));
+
+
+// funzione che rende maiuscole le iniziali di tutte le parole di una stringa
+function upperCaseWords (string) {
+    
+    const wordsArray = string.split(" ");
+    for (let i = 0; i < wordsArray.length; i++) {
+        wordsArray[i] = wordsArray[i][0].toUpperCase() + wordsArray[i].slice(1);
+    }
+    const result = wordsArray.join(" ");
+    return result;
+}
